5dc8b7f68e40fac733e6cae338d7d933dedc0d7d
Marcelo Peixoto <107879393+MarceloPeixoto20@users.noreply.github.com>
2023-11-03 14:28:47 -0300

    Versão Final
    Esse foi um projeto que eu fiz de cadastro de clientes para uma academia, com também a opção de você ter 2 tipos de usuários o normal ou admin e dependendo de qual tipo de usuário você teria permissões diferentes como o projeto foi feito so com clientes a única diferença consistia em que o admin podia criar usuários e o usuário nao.

---
+++ ProjetoAcademia/src/dal/ModuloConexao.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author MICRO
+ */
+public class ModuloConexao {
+
+    public static Connection conector() {      
+        String url = "jdbc:mysql://localhost:3306/projetoacademia";
+        String user = "root";
+        String password = "jana90le.";
+        
+        try {
+            Connection conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
